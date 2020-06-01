ef55464e7f1fb07a7bf72cbf020b14b4920eec9e
Peter Smith <peter@upstateinteractive.io>
2020-05-31 20:48:15 -0400

    chore: add more exercises

--- apps/programming-kitchen/src/app/exercise.service.ts
+++ apps/programming-kitchen/src/app/exercise.service.ts
@@ -8,17 +8,51 @@ import { BehaviorSubject, Observable } from 'rxjs';
 export class ExerciseService {
   private _sourceList: Exercise[] = [{
     id: 1,
-    name: 'MAPPU',
+    name: 'Handstand push-up negative',
     push: true,
-  },
-  {
+  }, {
     id: 2,
-    name: 'HSPU',
+    name: 'Hanstand to planche negative with hold',
     push: true,
-  },
-  {
+  }, {
     id: 3,
-    name: 'OAC',
+    name: 'Band planche',
+    push: true
+  }, {
+    id: 4,
+    name: 'Weighted dips',
+    push: true
+  }, {
+    id: 5,
+    name: 'OAC negatives',
+    pull: true
+  }, {
+    id: 6,
+    name: 'Front lever raises top half',
+    pull: true,
+  }, {
+    id: 7,
+    name: 'Weighted Chin-ups',
+    pull: true
+  }, {
+    id: 8,
+    name: 'Front lever holds with band',
+    pull: true
+  }, {
+    id: 9,
+    name: 'MAPPU',
+    push: true
+  }, {
+    id: 10,
+    name: 'Pseudo planche push-ups with lift',
+    push: true
+  }, {
+    id: 11,
+    name: 'L-sit chin-ups',
+    pull: true
+  }, {
+    id: 12,
+    name: 'Archer rows',
     pull: true
   }];
 
