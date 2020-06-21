ffd342aaf9a3e0593c717ce6d5750e5a816254a2
Peter Smith <peter@upstateinteractive.io>
2020-06-21 16:47:19 -0400

    feat: stub input page

--- apps/body-factory/src/app/app.module.ts
+++ apps/body-factory/src/app/app.module.ts
@@ -3,12 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { InputPageComponent } from './input-page/input-page.component';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, InputPageComponent],
   imports: [
+    MatCardModule,
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    RouterModule.forRoot(
+      [
+        {
+          path: '',
+          component: InputPageComponent,
+        },
+      ],
+      { initialNavigation: 'enabled' }
+    ),
   ],
   providers: [],
   bootstrap: [AppComponent],
