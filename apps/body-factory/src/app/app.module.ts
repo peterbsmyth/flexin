df2a7e2e05df06b261f4dbb56753c35dd33eac24
Peter Smith <peter@upstateinteractive.io>
2020-06-28 10:05:38 -0400

    feat: mock input page

--- apps/body-factory/src/app/app.module.ts
+++ apps/body-factory/src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { InputPageComponent } from './input-page/input-page.component';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [AppComponent, InputPageComponent],
   imports: [
+    BrowserAnimationsModule,
     MatCardModule,
+    MatButtonModule,
+    MatInputModule,
     BrowserModule,
     RouterModule.forRoot(
       [
--- apps/body-factory/src/app/input-page/input-page.component.ts
+++ apps/body-factory/src/app/input-page/input-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { mockSession, Session } from '@bod/models';
 
 @Component({
   selector: 'bod-input-page',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input-page.component.scss']
 })
 export class InputPageComponent implements OnInit {
+  session: Session = mockSession;
+  currentItemIndex =  0;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  arrayOfCount(n: number): any[] {
+    return Array(n);
+  }
+  
+  onGo(direction: string) {
+    const max = this.session.items.length - 1;
+    const canGoNext = this.currentItemIndex < max;
+    const canGoPrevious = this.currentItemIndex > 0;
+
+    if (direction === 'next' && canGoNext) {
+      this.currentItemIndex++;
+    } else if (direction === 'previous' && canGoPrevious) {
+      this.currentItemIndex--;
+    }
+  }
 }
