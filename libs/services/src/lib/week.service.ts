78171ad400de42979f5979cc13ccd13108137cac
Peter Smith <peter@upstateinteractive.io>
2020-06-29 08:11:16 -0400

    feat: mockWeek

--- libs/services/src/lib/week.service.ts
+++ libs/services/src/lib/week.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Week, Session } from '@bod/models';
+import { Week, Session, mockWeek } from '@bod/models';
 import { Subject, Observable, ReplaySubject } from 'rxjs';
 
 @Injectable({
@@ -7,11 +7,13 @@ import { Subject, Observable, ReplaySubject } from 'rxjs';
 })
 export class WeekService {
   private _lastWeekId = 0;
-  private _week: Week;
+  private _week: Week = mockWeek;
   private _week$: Subject<Week> = new ReplaySubject(1);
 
   get week$(): Observable<Week> { return this._week$.asObservable(); }
-  constructor() { }
+  constructor() {
+    this._week$.next(this._week);
+  }
 
   createWeek(number: number, sessions: Session[]): Week {
     this._week = {
