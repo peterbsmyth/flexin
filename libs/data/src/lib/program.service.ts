311a46062d3d0bc070ca11b65e96f1a17683ea46
Peter Smith <peter@upstateinteractive.io>
2020-09-05 20:06:47 +0100

    chore(data): new apiURL

--- libs/data/src/lib/program.service.ts
+++ libs/data/src/lib/program.service.ts
@@ -5,7 +5,7 @@ import { Subject, Observable, ReplaySubject } from 'rxjs';
 
 @Injectable()
 export class ProgramService {
-  private API_URL = 'https://30ef19c6-861b-43d6-8623-59fc434f085a.mock.pstmn.io';
+  private API_URL = 'https://9b6ba157-734d-42c3-8d2b-1d6d49f15793.mock.pstmn.io';
   private _program: Program = mockProgram;
   private _program$: Subject<Program> = new ReplaySubject(1);
   get program$(): Observable<Program> { return this._program$.asObservable(); }
--- libs/data/src/lib/week.service.ts
+++ libs/data/src/lib/week.service.ts
@@ -5,7 +5,7 @@ import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class WeekService {
-  private API_URL = 'https://30ef19c6-861b-43d6-8623-59fc434f085a.mock.pstmn.io';
+  private API_URL = 'https://9b6ba157-734d-42c3-8d2b-1d6d49f15793.mock.pstmn.io';
   private _lastWeekId = 0;
   private _week: Week = mockWeek;
   private _week$: Subject<Week> = new ReplaySubject(1);
