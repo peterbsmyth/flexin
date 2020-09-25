a4cd68206ead1a24b1f073c52b80ce6426ff6826
Peter Smith <peter@upstateinteractive.io>
2020-09-25 13:45:33 +0100

    fix(coaching): remove mockProgram

--- libs/coaching/domain/src/lib/infrastructure/program.data.service.ts
+++ libs/coaching/domain/src/lib/infrastructure/program.data.service.ts
@@ -1,20 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Week, Program, mockProgram } from '@bod/shared/models';
-import { Subject, Observable, ReplaySubject } from 'rxjs';
+import { Program } from '@bod/shared/models';
+import { Observable } from 'rxjs';
 import { environment } from '@bod/shared/environments';
 
 @Injectable()
 export class ProgramDataService {
   private API_URL = environment.API_URL;
-  private _program: Program = mockProgram;
-  private _program$: Subject<Program> = new ReplaySubject(1);
-  get program$(): Observable<Program> { return this._program$.asObservable(); }
-  constructor(
-    private http: HttpClient
-  ) {
-    this._program$.next(this._program);
-  }
+  constructor(private http: HttpClient) {}
 
   getAll(): Observable<Program[]> {
     return this.http.get<Program[]>(`${this.API_URL}/programs`);
