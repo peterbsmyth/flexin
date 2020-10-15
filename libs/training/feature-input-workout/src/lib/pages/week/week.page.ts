5ed231cc9350ca54fcb8002437f1ae7c1ccd14d8
Peter Smith <peter@upstateinteractive.io>
2020-10-15 10:44:13 +0100

    fix(training): enable view update when other than id changes

--- libs/training/feature-input-workout/src/lib/pages/week/week.page.ts
+++ libs/training/feature-input-workout/src/lib/pages/week/week.page.ts
@@ -25,7 +25,6 @@ export class WeekPage implements OnInit, OnDestroy {
       filter((w) => {
         return !!w?.program;
       }),
-      distinctUntilKeyChanged('id')
     );
 
     this.weekStatisticsState.error$
