e37dd083b936a626d2dd8b41fbebda73fd965e83
Peter Smith <peter@upstateinteractive.io>
2020-11-06 11:53:08 +0000

    fix(training): import error

--- libs/training/domain/src/lib/application/manage-meal-plans.facade.ts
+++ libs/training/domain/src/lib/application/manage-meal-plans.facade.ts
@@ -1,24 +1,3 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-
-import { MealPlan } from '@bod/shared/models';
-import { MealPlanDataService } from '../infrastructure/meal-plan.data.service';
-
 @Injectable({ providedIn: 'root' })
-export class ManageMealPlansFacade {
-  private mealPlanListSubject = new BehaviorSubject<MealPlan[]>([]);
-  mealPlanList$ = this.mealPlanListSubject.asObservable();
-
-  constructor(private mealPlanDataService: MealPlanDataService) {}
-
-  load(): void {
-    this.mealPlanDataService.load().subscribe(
-      (mealPlanList) => {
-        this.mealPlanListSubject.next(mealPlanList);
-      },
-      (err) => {
-        console.error('err', err);
-      }
-    );
-  }
-}
+export class ManageMealPlansFacade {}
