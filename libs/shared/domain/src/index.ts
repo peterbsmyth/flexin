48189d6f41f875308d203eb026bccad921d1a40e
Peter Smith <peter@upstateinteractive.io>
2020-11-22 10:39:01 +0000

    fix(misc): removing unused modules, update ddd

--- libs/shared/domain/src/index.ts
+++ libs/shared/domain/src/index.ts
@@ -1,3 +1,2 @@
-export * from './lib/shared-domain.module';
 export * from './lib/application/network-status.facade';
 export * from './lib/network-status.module';
--- libs/shared/utils/src/index.ts
+++ libs/shared/utils/src/index.ts
@@ -1,3 +1,2 @@
-export * from './lib/utils.module';
 export * from './lib/on-change.decorator';
 export * from './lib/isBoardEmpty';
